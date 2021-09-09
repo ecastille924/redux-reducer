d3421cf47cbc37632cfa844c09acacb46861aca2
Erick Castille <ecastille924@gmail.com>
2021-09-08 20:17:36 -0400

    commit tree 992e7235eef3efc29b24ca0438d64fcda393dd89

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,28 @@
-export function manageFriends(state, action){
+export function manageFriends(state= { friends:[] }, action){
+    switch (action.type){
+        
+        case "ADD_FRIEND":
+            return  {...state, friends: [...state.friends, action.friend]}
+        
+        case "REMOVE_FRIEND":
+            const friendToRemove = state.friends.findIndex(friend => friend.id === action.id)
+            const updatedFriends = [
+                ...state.friends.slice(0, friendToRemove), 
+                ...state.friends.slice(friendToRemove + 1)
+                ]
+            return {...state, friends: updatedFriends}
+        
+        default:
+            return state;
+    }
 }
+
+
+// export function managePresents(state= { numberOfPresents: 0 }, action){
+//     switch (action.type){
+//         case "INCREASE":
+//         return {numberOfPresents: state.numberOfPresents + 1}
+//     default:
+//         return state;
+//     }
+// }
